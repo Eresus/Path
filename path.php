ef5a2760c6631e3907bdcc2a07820a3fca6005e4
Михаил Красильников <m.krasilnikov@yandex.ru>
2010-07-06 12:19:14 +0000

    [ИСПРАВЛЕНО] 0000380: Неправильный вызов конструктора

--- path.php
+++ path.php
@@ -2,11 +2,9 @@
 /**
  * "Хлебные крошки"
  *
- * Eresus 2.10rc2
- *
  * Строка с местом положения на сайте.
  *
- * @version 2.00
+ * @version 2.01
  *
  * @copyright 2005, ProCreat Systems, http://procreat.ru/
  * @copyright 2007, Eresus Group, http://eresus.ru/
@@ -41,12 +39,12 @@
  */
 class Path extends Plugin
 {
-  var $version = '2.00';
-  var $kernel = '2.10rc2';
-	var $title = '"Хлебные крошки"';
-  var $description = 'Строка с местом положения на сайте';
-	var $type = 'client';
-  var $settings = array (
+  public $version = '2.01a';
+  public $kernel = '2.10rc2';
+	public $title = '"Хлебные крошки"';
+  public $description = 'Строка с местом положения на сайте';
+	public $type = 'client';
+  public $settings = array (
     'prefix' => '',
     'delimiter' => '&nbsp;&raquo;&nbsp;',
     'link' => '<a href="$(url)" title="$(description)">$(caption)</a>',
@@ -60,23 +58,23 @@ class Path extends Plugin
    *
    * @var array
    */
-  var $path = array();
+  public $path = array();
 
   /**
    * Уровень вложенности текущего раздела
    *
    * @var int
    */
-  var $level = -1;
+  public $level = -1;
 
   /**
    * Конструктор
    *
    * @return Path
    */
-  function Path()
+  public function __construct()
   {
-    parent::Plugin();
+    parent::__construct();
     $this->listenEvents('clientOnURLSplit', 'clientOnPageRender');
   }
   //-----------------------------------------------------------------------------
