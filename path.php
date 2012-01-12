c39f24f1d86bed49c318c0925b7064233fa4f3c3
Александр Гаврилюк <olex@dvaslona.ru>
2012-01-12 03:36:19 +0000

    Добавлена возможность не выводить скрытые разделы - http://bugs.eresus.ru/view.php?id=773

--- path.php
+++ path.php
@@ -44,102 +44,119 @@ class Path extends Plugin
 	 *
 	 * @var string
 	 */
-  public $version = '2.01a';
+	public $version = '2.01a';
 
-  public $kernel = '2.10';
+	public $kernel = '2.10';
 	public $title = 'Положение на сайте';
-  public $description = 'Строка с местом положения на сайте';
+	public $description = 'Строка с местом положения на сайте';
 	public $type = 'client';
-  public $settings = array (
-    'prefix' => '',
-    'delimiter' => '&nbsp;&raquo;&nbsp;',
-    'link' => '<a href="$(url)" title="$(description)">$(caption)</a>',
-    'current' => '$(caption)',
-    'levelMin' => 0,
-    'levelMax' => 0,
-  );
+	public $settings = array (
+		'prefix' => '',
+		'delimiter' => '&nbsp;&raquo;&nbsp;',
+		'link' => '<a href="$(url)" title="$(description)">$(caption)</a>',
+		'current' => '$(caption)',
+		'levelMin' => 0,
+		'levelMax' => 0,
+		'noShowHidden' => 1,
+	);
 
-  /**
-   * Хранит элементы пути
-   *
-   * @var array
-   */
-  public $path = array();
+	/**
+	 * Хранит элементы пути
+	 *
+	 * @var array
+	 */
+	public $path = array();
 
-  /**
-   * Уровень вложенности текущего раздела
-   *
-   * @var int
-   */
-  public $level = -1;
+	/**
+	 * Уровень вложенности текущего раздела
+	 *
+	 * @var int
+	 */
+	public $level = -1;
 
-  /**
-   * Конструктор
-   *
-   * @return Path
-   */
-  public function __construct()
-  {
-    parent::__construct();
-    $this->listenEvents('clientOnURLSplit', 'clientOnPageRender');
-  }
-  //-----------------------------------------------------------------------------
+	/**
+	 * Конструктор
+	 *
+	 * @return Path
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->listenEvents('clientOnURLSplit', 'clientOnPageRender');
+	}
+	//-----------------------------------------------------------------------------
 
-  function settings()
-  {
-  	global $page;
+	function settings()
+	{
+		global $page;
 
-    $form = array(
-      'name' => 'Settings',
-      'caption' => $this->title.' '.$this->version,
-      'width' => '500px',
-      'fields' => array (
-        array('type'=>'hidden','name'=>'update', 'value'=>$this->name),
-        array('type'=>'edit','name'=>'prefix','label'=>'Префикс пути','width'=>'100%'),
-        array('type'=>'edit','name'=>'delimiter','label'=>'Разделитель разделов','width'=>'100%'),
-        array('type'=>'edit','name'=>'link','label'=>'Шаблон ссылки','width'=>'100%'),
-        array('type'=>'edit','name'=>'current','label'=>'Для текущей страницы','width'=>'100%'),
-        array('type'=>'edit','name'=>'levelMin','label'=>'Мин.вложенность','width'=>'20px','comment'=>' 0 - любая'),
-        array('type'=>'edit','name'=>'levelMax','label'=>'Макс.вложенность','width'=>'20px','comment'=>' 0 - любая'),
-        array('type'=>'divider'),
-        array('type'=>'text','value'=>"Заменяет макрос $(Path) на строку с текущим положением на сайте."),
-        array('type'=>'divider'),
-      ),
-      'buttons' => array('ok', 'apply', 'cancel'),
-    );
-    $result = $page->renderForm($form, $this->settings);
-    return $result;
-  }
-  //-----------------------------------------------------------------------------
+		$form = array(
+			'name' => 'Settings',
+			'caption' => $this->title.' '.$this->version,
+			'width' => '500px',
+			'fields' => array (
+				array('type'=>'hidden','name'=>'update', 'value'=>$this->name),
+				array('type'=>'edit','name'=>'prefix','label'=>'Префикс пути','width'=>'100%'),
+				array('type'=>'edit','name'=>'delimiter','label'=>'Разделитель разделов','width'=>'100%'),
+				array('type'=>'edit','name'=>'link','label'=>'Шаблон ссылки','width'=>'100%'),
+				array('type'=>'edit','name'=>'current','label'=>'Для текущей страницы','width'=>'100%'),
+				array('type'=>'edit','name'=>'levelMin','label'=>'Мин.вложенность','width'=>'20px',
+				'comment'=>' 0 - любая'),
+				array('type'=>'edit','name'=>'levelMax','label'=>'Макс.вложенность','width'=>'20px',
+				'comment'=>' 0 - любая'),
+				array('type'=>'checkbox','name'=>'noShowHidden','label'=>'Не показывать скрытые разделы'),
+				array('type'=>'divider'),
+				array('type'=>'text',
+				'value'=>"Заменяет макрос $(Path) на строку с текущим положением на сайте."),
+				array('type'=>'divider'),
+			),
+			'buttons' => array('ok', 'apply', 'cancel'),
+		);
+		$result = $page->renderForm($form, $this->settings);
+		return $result;
+	}
+	//-----------------------------------------------------------------------------
 
-  function clientOnPageRender($text)
-  {
-    global $page;
+	function clientOnPageRender($text)
+	{
 
-    if (
-      (!$this->settings['levelMin'] || ($this->level >= $this->settings['levelMin']))
-      &&
-      (!$this->settings['levelMax'] || ($this->level <= $this->settings['levelMax']))
-    ) {
-      $result = array();
-      for($i = 0; $i < count($this->path); $i++) {
-        $item = $this->path[$i];
-        $item['url'] = httpRoot.$item[$this->name.'_url'];
-        $template = ($i == count($this->path)-1)?$this->settings['current']:$this->settings['link'];
-        $result[] = $this->replaceMacros($template, $item);
-      }
-      $result = implode($this->settings['delimiter'], $result);
-      $result = str_replace('$(Path)', $this->settings['prefix'].$result, $text);
-    } else $result = str_replace('$(Path)', '', $text);
-    return $result;
-  }
-  //-----------------------------------------------------------------------------
+		if (
+			(!$this->settings['levelMin'] || ($this->level >= $this->settings['levelMin']))
+			&&
+			(!$this->settings['levelMax'] || ($this->level <= $this->settings['levelMax']))
+		)
+		{
+			$result = array();
+			for ($i = 0; $i < count($this->path); $i++)
+			{
+				$item = $this->path[$i];
+				$item['url'] = httpRoot.$item[$this->name.'_url'];
+				$template = ($i == count($this->path)-1)?$this->settings['current']:$this->settings['link'];
+				$result[] = $this->replaceMacros($template, $item);
+			}
+			$result = implode($this->settings['delimiter'], $result);
+			$result = str_replace('$(Path)', $this->settings['prefix'].$result, $text);
+		}
+		else
+		{
+			$result = str_replace('$(Path)', '', $text);
+		}
+		return $result;
+	}
+	//-----------------------------------------------------------------------------
 
-  function clientOnURLSplit($item, $url)
-  {
-    $item[$this->name.'_url'] = ($url == 'main/')?'':$url;
-    $this->path[] = $item;
-    $this->level++;
-  }
-  //-----------------------------------------------------------------------------
+	function clientOnURLSplit($item, $url)
+	{
+		$item[$this->name.'_url'] = ($url == 'main/')?'':$url;
+		if ($item['visible'] == 0 && $this->settings['noShowHidden'] == 1)
+		{
+			return;	
+		}
+		else
+		{
+			$this->path[] = $item;
+			$this->level++;		
+		}
+	}
+	//-----------------------------------------------------------------------------
 }
